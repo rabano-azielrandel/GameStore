13b8048510d4986d208a0c8c5747ef1c560cb981
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-16 23:37:33 +0800

    configure logging levels in appsettings.json

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e097c85a395746e75f59d85d5b11a1bf68d45c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98672a4932b8c9aa9d02045bcb6def16043babd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
