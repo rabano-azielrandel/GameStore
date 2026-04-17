4be4158fc18eb68b47038f49fa18b4f463e8c13c
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-17 10:56:33 +0800

    apply async approach

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5906051a523b0c369057824b32311ddedae53596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa901688b9a80e60fd1b5c8a15a455d7f320ba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
